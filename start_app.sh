1e297a4b917dc6e50dc5f0d34a2cc3f7f61a6d96
Kamwoo Lee <klee16@worldbank.org>
2025-10-22 15:27:51 +0000

    changed port

--- start_app.sh
+++ start_app.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 source /usr/local/anaconda3/etc/profile.d/conda.sh
-conda activate ai_for_data
+conda activate ai_for_data_2
 
 # export GRADIO_TEMP_DIR="tmp"
 
@@ -13,7 +13,7 @@ fi
 CURRENT_DATE=$(date +%F)
 LOG_FILE="${LOG_DIR}/${CURRENT_DATE}.log"
 
-uvicorn ai_for_data_playground_main:app --port 8082 --host 0.0.0.0 --ssl-keyfile=../../ssl/w1lxscirender02.worldbank.org.key --ssl-certfile=../../ssl/w1lxscirender02.worldbank.org.pem > "$LOG_FILE" 2>&1 &
+uvicorn ai_for_data_playground_main:app --port 8080 --host 0.0.0.0 --ssl-keyfile=../../ssl/w1lxscirender02.worldbank.org.key --ssl-certfile=../../ssl/w1lxscirender02.worldbank.org.pem > "$LOG_FILE" 2>&1 &
 # uvicorn ai_for_data_playground_main:app --port 8082 --host 0.0.0.0 --ssl-keyfile=../../ssl/w1lxscirender02.worldbank.org.key --ssl-certfile=../../ssl/w1lxscirender02.worldbank.org.pem
 # uvicorn ai_for_data_playground_main:app --port 8082 --host 0.0.0.0 --ssl-keyfile=/etc/ssl/private/w1lxscirender02.worldbank.org.key --ssl-certfile=/etc/ssl/certs/w1lxscirender02.worldbank.org.pem
 echo "AI for Data Playground started, logging to $LOG_FILE"
